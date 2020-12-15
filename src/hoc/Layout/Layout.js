8b859a999985ce00f623b6d085d14cb9f6125c1d
Joe Alongi <collectedview@gmail.com>
2020-12-14 19:09:38 -0500

    Auth & Logout
    Authentication & Logout components convertedt to useHooks.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-import Auxiliary from '../Auxiliary/Auxiliary';
-import classes from './Layout.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import Auxiliary from "../Auxiliary/Auxiliary";
+import classes from "./Layout.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
-const layout = props => {
-    const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+const layout = (props) => {
+  const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
 
-    const sideDrawerClosedHandler = () => {
-        setSideDrawerIsVisible(false);
-    }
+  const sideDrawerClosedHandler = () => {
+    setSideDrawerIsVisible(false);
+  };
 
-    const sideDrawerToggleHandler = () => {
-        setSideDrawerIsVisible(!sideDrawerIsVisible);
-    }
+  const sideDrawerToggleHandler = () => {
+    setSideDrawerIsVisible(!sideDrawerIsVisible);
+  };
 
-        return (
-            <Auxiliary>
-                <Toolbar
-                    isAuth={props.isAuthenticated}
-                    drawerToggleClicked={sideDrawerToggleHandler} />
-                <SideDrawer
-                    isAuth={props.isAuthenticated}
-                    open={sideDrawerIsVisible}
-                    closed={sideDrawerClosedHandler} />
-                <main className={classes.Content}>
-                    {props.children}
-                </main>
-            </Auxiliary>
-        )
-}
-
-const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.auth.token !== null
-    };
+  return (
+    <Auxiliary>
+      <Toolbar
+        isAuth={props.isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler}
+      />
+      <SideDrawer
+        isAuth={props.isAuthenticated}
+        open={sideDrawerIsVisible}
+        closed={sideDrawerClosedHandler}
+      />
+      <main className={classes.Content}>{props.children}</main>
+    </Auxiliary>
+  );
 };
 
-export default connect( mapStateToProps )( layout );
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(layout);
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Modal from '../../components/UI/Modal/Modal';
-import Auxiliary from '../Auxiliary/Auxiliary';
+import Modal from "../../components/UI/Modal/Modal";
+import Auxiliary from "../Auxiliary/Auxiliary";
 
-const withErrorHandler = ( WrappedComponent, axios ) => {
-    return props => {
-        const [error, setError] = useState(null);
+const withErrorHandler = (WrappedComponent, axios) => {
+  return (props) => {
+    const [error, setError] = useState(null);
 
-            const reqInterceptor = axios.interceptors.request.use( req => {
-                setError(null);
-                return req;
-            } );
-            const resInterceptor = axios.interceptors.response.use( res => res, err => {
-                setError(err);
-            } );
+    const reqInterceptor = axios.interceptors.request.use((req) => {
+      setError(null);
+      return req;
+    });
+    const resInterceptor = axios.interceptors.response.use(
+      (res) => res,
+      (err) => {
+        setError(err);
+      }
+    );
 
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.response.eject(resInterceptor);
-            };
-        }, [reqInterceptor, resInterceptor]);
+    useEffect(() => {
+      return () => {
+        axios.interceptors.request.eject(reqInterceptor);
+        axios.interceptors.response.eject(resInterceptor);
+      };
+    }, [reqInterceptor, resInterceptor]);
 
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
+    const errorConfirmedHandler = () => {
+      setError(null);
+    };
 
-            return (
-                <Auxiliary>
-                    <Modal
-                        show={error}
-                        modalClosed={errorConfirmedHandler}>
-                        {error ? error.message : null}
-                    </Modal>
-                    <WrappedComponent {...props} />
-                </Auxiliary>
-            );
-
-    }
-}
+    return (
+      <Auxiliary>
+        <Modal show={error} modalClosed={errorConfirmedHandler}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </Auxiliary>
+    );
+  };
+};
 
 export default withErrorHandler;
